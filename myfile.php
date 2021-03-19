598be58f7fa47564a9c3bb5dfdfdc32f9409b59c
rookjeknaja1 <DSITSY@gmail.com>
2021-03-19 16:32:02 +0700

    lab10

---
+++ myfile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+
+<body>
+
+<?php
+    $name=$_POST["name"];
+    $surname=$_POST["surname"];
+?>
+
+<h4>My Name is <?=$name?></h4>
+<h4>My Surname is <?=$surname?></h4>
+
+</body>
+</html>
---
+++ writefile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+
+<body>
+
+<form method="POST" action="myfile.php">
+    <p>
+        Name : <input type="text" name="name">
+    </p>
+    <p>
+        Surname : <input type="text" name="surname">
+    </p>
+    <p>
+        <button type="submit">Save</button>
+    </p>
+  
+</form> 
+
+</body>
+</html>
